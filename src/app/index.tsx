f749d7f0cbbdb1396e20d59ad3a6ffe8d36fb831
Lucas Rocha <lucas.lgr.01@gmail.com>
2026-01-16 17:01:49 -0300

    criando list

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,8 @@
 import { View } from "react-native"
 
 import { HomeHeader } from "@/components/HomeHeader"
+import { Target } from "@/components/Target"
+import { List } from "@/components/List"
 
 const summary = {
 	total: "R$ 2.680,00",
@@ -8,10 +10,44 @@ const summary = {
 	output: { label: "Saídas", value:"-R$ 883.65"},
 }
 
+const targets = [
+	{
+		id: "1",
+		name:"Apple Watch",
+		current: "50%",
+		percentage:"R$ 580,00",
+		target: "R$ 1.790,00",
+	},
+	{
+		id: "2",
+		name:"Comprar uma cadeira ergonômica",
+		current: "75%",
+		percentage:"R$900,00",
+		target: "R$ 1.200,00",
+	},
+	{
+		id: "3",
+		name:"Fazer uma viagem para o Rio de Janeiro",
+		current: "75%",
+		percentage:"R$ 1.200,00",
+		target: "R$ 3.000,00",
+	}
+]
+
 export default function index(){
 	return (
 		<View style={{ flex: 1}}>
 			<HomeHeader data={ summary } />
+
+			<List
+				title="Metas" 
+				data={[]}
+				keyExtractor={(item) => item.id}
+				renderItem={({ item }) => <Target data={item}/>}
+				emptyMessage="Nenhuma meta. Clique em nova meta para criar."
+				containerStyle={{ paddingHorizontal: 24}}
+
+			/>
 		</View>
 	)
 }
