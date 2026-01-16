d00ab884d226ac28ff76e71eac8ccd6b17be8d48
Lucas Rocha <lucas.lgr.01@gmail.com>
2026-01-16 13:53:28 -0300

    configura e implementa expo router

---
+++ src/app/index.tsx
@@ -0,0 +1,25 @@
+import { router } from "expo-router"
+import { Button, Text, View } from "react-native"
+
+export default function index(){
+	return (
+		<View style={{ flex: 1, justifyContent: "center"}}>
+			<Text>Expo Router</Text>
+
+			<Button 
+				title="Nova Meta"
+				onPress={() => router.navigate("/target")}
+			/>
+
+			<Button 
+				title="Transação"
+				onPress={() => router.navigate("/transaction/132")}
+			/>
+
+			<Button 
+				title="Progresso"
+				onPress={() => router.navigate("/in-progress/12")}
+			/>
+		</View>
+	)
+}
