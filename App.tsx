d6658059bf3eeefa06de32ceb43a8bf814024e94
Lucas Rocha <lucas.lgr.01@gmail.com>
2026-01-16 00:37:35 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import { colors } from "@/theme/colors";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Oi, React Native</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.blue[200],
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- android/app/src/main/java/com/lucasdu/target/MainActivity.kt
+++ android/app/src/main/java/com/lucasdu/target/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.target
+package com.lucasdu.target
 
 import android.os.Build
 import android.os.Bundle
