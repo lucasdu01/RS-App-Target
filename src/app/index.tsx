98777ded511f8d2065229b41b7176e638c140c8a
Lucas Rocha <lucas.lgr.01@gmail.com>
2026-01-16 16:21:35 -0300

    Construindo home

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,26 +1,11 @@
-import { fontFamily } from "@/theme/fontFamily"
-import { router } from "expo-router"
-import { Button, Text, View } from "react-native"
+import { View } from "react-native"
+
+import { HomeHeader } from "@/components/HomeHeader"
 
 export default function index(){
 	return (
-		<View style={{ flex: 1, justifyContent: "center"}}>
-			<Text style={{ fontFamily: fontFamily.bold, fontSize: 34}}>Expo Router</Text>
-
-			<Button 
-				title="Nova Meta"
-				onPress={() => router.navigate("/target")}
-			/>
-
-			<Button 
-				title="Transação"
-				onPress={() => router.navigate("/transaction/132")}
-			/>
-
-			<Button 
-				title="Progresso"
-				onPress={() => router.navigate("/in-progress/12")}
-			/>
+		<View style={{ flex: 1}}>
+			<HomeHeader data={{ total: "R$2.680,00" }} />
 		</View>
 	)
 }
---
+++ src/components/HomeHeader/index.tsx
@@ -0,0 +1,46 @@
+import { View, Text } from "react-native"
+import { LinearGradient } from "expo-linear-gradient";
+
+import { styles } from "./styles";
+import { colors } from "@/theme/colors"
+
+import { Separator } from "@/components/Separator"
+import { Summary } from "@/components/Summary"
+
+export type HomeHeaderProps = {
+	total: string
+}
+
+type Props = {
+	data: HomeHeaderProps
+
+}
+
+export function HomeHeader({ data }: Props) {
+	return (
+  		<LinearGradient
+			colors={[colors.blue[500], colors.blue[800]]}
+    		style={styles.container}
+    	>
+			<View>
+				<Text style={styles.label}>Total que você possui</Text>
+				<Text style={styles.total}>{data.total}</Text>
+			</View>
+
+			<Separator color={colors.blue[400]}/>
+
+			<View style={styles.summary}>
+				<Summary
+					data={{ label: "Entradas", value:"R$ 6,184.90"}}
+					icon={{ name: "arrow-upward", color: colors.green[500]}}
+				/>
+
+				<Summary
+					isLeft
+					data={{ label: "Saídas", value:"-R$ 883.65"}}
+					icon={{ name: "arrow-downward", color: colors.red[400]}}
+				/>
+			</View>
+		</LinearGradient>
+  	)
+}
