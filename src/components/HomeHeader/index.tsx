fd88ae6370469293c854a954e1cc98d8d893b5ef
Lucas Rocha <lucas.lgr.01@gmail.com>
2026-01-16 16:27:16 -0300

    reaproveitando tipos

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -5,15 +5,16 @@ import { styles } from "./styles";
 import { colors } from "@/theme/colors"
 
 import { Separator } from "@/components/Separator"
-import { Summary } from "@/components/Summary"
+import { Summary, SummaryProps } from "@/components/Summary"
 
 export type HomeHeaderProps = {
 	total: string
+	input: SummaryProps
+	output: SummaryProps
 }
 
 type Props = {
 	data: HomeHeaderProps
-
 }
 
 export function HomeHeader({ data }: Props) {
@@ -31,13 +32,13 @@ export function HomeHeader({ data }: Props) {
 
 			<View style={styles.summary}>
 				<Summary
-					data={{ label: "Entradas", value:"R$ 6,184.90"}}
+					data={data.input}
 					icon={{ name: "arrow-upward", color: colors.green[500]}}
 				/>
 
 				<Summary
 					isLeft
-					data={{ label: "Saídas", value:"-R$ 883.65"}}
+					data={data.output}
 					icon={{ name: "arrow-downward", color: colors.red[400]}}
 				/>
 			</View>
