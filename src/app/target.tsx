fcb4806d071ad17d498cfaadb8f35f89db254d15
Lucas Rocha <lucas.lgr.01@gmail.com>
2026-01-20 20:32:37 -0300

    Componente CurrencyInput

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,9 +1,9 @@
-import { router } from "expo-router"
 import { View } from "react-native"
 
 import { PageHeader } from "@/components/PageHeader"
 import { Input } from "@/components/Input"
 import { Button } from "@/components/Button"
+import { CurrencyInput } from "@/components/CurrencyInput"
 
 export default function Target(){
 	return(
@@ -14,7 +14,13 @@ export default function Target(){
 			/>
 
 			<View style={{ marginTop: 32, gap: 24}}>
-				<Input label="Nome da meta" placeholder="Ex.:  Viagem para praia, Apple Watch"/>
+				<Input
+					label="Nome da meta"
+					placeholder="Ex.:  Viagem para praia, Apple Watch"
+				/>
+
+				<CurrencyInput label="Valor alvo" value={0}/>
+
 				<Button title="Salvar"/>
 			</View>
 		</View>
