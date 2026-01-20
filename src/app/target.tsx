1ff2c448b37bf730f199c53ebfcb9ddcfa0ba32b
Lucas Rocha <lucas.lgr.01@gmail.com>
2026-01-20 20:24:26 -0300

    componente Input

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,7 +1,9 @@
 import { router } from "expo-router"
-import { Button, View } from "react-native"
+import { View } from "react-native"
 
 import { PageHeader } from "@/components/PageHeader"
+import { Input } from "@/components/Input"
+import { Button } from "@/components/Button"
 
 export default function Target(){
 	return(
@@ -10,11 +12,11 @@ export default function Target(){
 				title="Meta"
 				subtitle="Economize para alcançar sua meta financeira."
 			/>
-			<Button
-				title="Voltar"
-				onPress={() => router.back()}
-			/>
-			
+
+			<View style={{ marginTop: 32, gap: 24}}>
+				<Input label="Nome da meta" placeholder="Ex.:  Viagem para praia, Apple Watch"/>
+				<Button title="Salvar"/>
+			</View>
 		</View>
 	)
 }
