d7e8e73863ac0462598a6757da3042ba28ec3942
Lucas Rocha <lucas.lgr.01@gmail.com>
2026-01-22 22:07:24 -0300

    calculando transacoes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,63 +1,84 @@
-import { View, StatusBar } from "react-native"
-import { router } from "expo-router"
+import { useCallback } from 'react';
+import { View, StatusBar, Alert } from 'react-native';
+import { router, useFocusEffect } from 'expo-router';
 
-import { HomeHeader } from "@/components/HomeHeader"
-import { Target } from "@/components/Target"
-import { List } from "@/components/List"
-import { Button } from "@/components/Button"
+import { HomeHeader } from '@/components/HomeHeader';
+import { Target } from '@/components/Target';
+import { List } from '@/components/List';
+import { Button } from '@/components/Button';
+
+import { useTargetDatabase } from "@/database/useTargetDatabase"
 
 const summary = {
-	total: "R$ 2.680,00",
-	input: { label: "Entradas", value:"R$ 6,184.90"},
-	output: { label: "Saídas", value:"-R$ 883.65"},
-}
+  total: 'R$ 2.680,00',
+  input: { label: 'Entradas', value: 'R$ 6,184.90' },
+  output: { label: 'Saídas', value: '-R$ 883.65' },
+};
 
 const targets = [
-	{
-		id: "1",
-		name:"Apple Watch",
-		current: "50%",
-		percentage:"R$ 580,00",
-		target: "R$ 1.790,00",
-	},
-	{
-		id: "2",
-		name:"Comprar uma cadeira ergonômica",
-		current: "75%",
-		percentage:"R$900,00",
-		target: "R$ 1.200,00",
-	},
-	{
-		id: "3",
-		name:"Fazer uma viagem para o Rio de Janeiro",
-		current: "75%",
-		percentage:"R$ 1.200,00",
-		target: "R$ 3.000,00",
-	}
-]
+  {
+    id: '1',
+    name: 'Apple Watch',
+    current: '50%',
+    percentage: 'R$ 580,00',
+    target: 'R$ 1.790,00',
+  },
+  {
+    id: '2',
+    name: 'Comprar uma cadeira ergonômica',
+    current: '75%',
+    percentage: 'R$900,00',
+    target: 'R$ 1.200,00',
+  },
+  {
+    id: '3',
+    name: 'Fazer uma viagem para o Rio de Janeiro',
+    current: '75%',
+    percentage: 'R$ 1.200,00',
+    target: 'R$ 3.000,00',
+  },
+];
 
-export default function index(){
-	return (
-		<View style={{ flex: 1}}>
-			<StatusBar barStyle="light-content" />
-			<HomeHeader data={ summary } />
+export default function index() {
+  const targetDatabase = useTargetDatabase()
 
-			<List
-				title="Metas" 
-				data={targets}
-				keyExtractor={(item) => item.id}
-				renderItem={({ item }) => (
-					<Target 
-						data={item}
-						onPress={() => router.navigate(`/in-progress/${item.id}`)}
-					/>
-				)}
-				emptyMessage="Nenhuma meta. Clique em nova meta para criar."
-				containerStyle={{ paddingHorizontal: 24}}
-			/>
-			<View style={{ padding: 24, paddingBottom: 32 }}>
-				<Button title="Nova Meta" onPress={() => router.navigate("/target")}/>
-			</View>
-		</View>
-	)
+  async function fetchTargets() {
+    try {
+      const response = await targetDatabase.listBySavedValue()
+      console.log(response)
+    } catch (error) {
+      Alert.alert("Erro", "Não foi possível carregar as metas.")
+      console.log(error)
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchTargets()
+    },[])
+  )
+
+  return (
+    <View style={{ flex: 1 }}>
+      <StatusBar barStyle="light-content" />
+      <HomeHeader data={summary} />
+
+      <List
+        title="Metas"
+        data={targets}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <Target
+            data={item}
+            onPress={() => router.navigate(`/in-progress/${item.id}`)}
+          />
+        )}
+        emptyMessage="Nenhuma meta. Clique em nova meta para criar."
+        containerStyle={{ paddingHorizontal: 24 }}
+      />
+      <View style={{ padding: 24, paddingBottom: 32 }}>
+        <Button title="Nova Meta" onPress={() => router.navigate('/target')} />
+      </View>
+    </View>
+  );
 }
--- src/database/useTargetDatabase.ts
+++ src/database/useTargetDatabase.ts
@@ -1,22 +1,50 @@
-import { useSQLiteContext } from "expo-sqlite"
+import { useSQLiteContext } from 'expo-sqlite';
 
 export type TargetCreate = {
-	name: string,
-	amount: number,
-}
+  name: string;
+  amount: number;
+};
+
+export type TargetResponse = {
+  id: number;
+  name: string;
+  amount: number;
+  current: number;
+  percentage: number;
+  created_at: Date;
+  updated_at: Date;
+};
 
 export function useTargetDatabase() {
-	const database = useSQLiteContext()
+  const database = useSQLiteContext();
 
-	async function create(data: TargetCreate) {
-		const statement = await database.prepareAsync("INSERT INTO targets (name, amount) VALUES ($name, $amount)")
-		statement.executeAsync({
-			$name: data.name,
-			$amount:data.amount
-		})
-	}
+  async function create(data: TargetCreate) {
+    const statement = await database.prepareAsync(
+      'INSERT INTO targets (name, amount) VALUES ($name, $amount)'
+    );
+    statement.executeAsync({
+      $name: data.name,
+      $amount: data.amount,
+    });
+  }
 
-	return {
-		create,
-	}
+  function listBySavedValue() {
+    return database.getAllAsync<TargetResponse>(`
+		SELECT
+			targets.id,
+			targets.name,
+			targets.amount,
+      COALESCE(SUM(transactions.amount), 0) AS current,
+      COALESCE((SUM(transactions.amount) / targets.amount) * 100, 0) AS percentage
+		FROM targets
+    LEFT JOIN transactions ON targets.id = transactions.target_id
+    GROUP BY targets.id, targets.name, targets.amount
+    ORDER BY current DESC
+		`);
+  }
+
+  return {
+    create,
+    listBySavedValue,
+  };
 }
